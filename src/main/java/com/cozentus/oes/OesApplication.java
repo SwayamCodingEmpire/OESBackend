9880b4d6ec8e1083b0d17cc889cff5ed05be3d59
CoderSPM <mohantyswayam2001@gmail.com>
2025-06-17 14:29:36 +0530

    Initial commit

---
+++ src/main/java/com/cozentus/oes/OesApplication.java
@@ -0,0 +1,13 @@
+package com.cozentus.oes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OesApplication.class, args);
+	}
+
+}
